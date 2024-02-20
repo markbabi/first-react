2ecdd33dbb907ff531fb8cebc1b20af08e00e71f
Mark Babirad <mbabirad@brainstation.io>
2024-02-20 14:20:48 -0800

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Demo: React</h1>
+        <p>This is almost as cool as Mark!!</p>
+      </header>
+      <main className="main">
+        <h2>Other stuff... I kinda miss the old DOM</h2>
+        <p>NO YOU DONT</p>
+      </main>
+      <footer>
+        <p>This be the footer</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
